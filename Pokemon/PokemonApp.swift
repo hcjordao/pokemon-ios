7053a5c4d435b78f4dce4a57050ff6ef5dc473fd
Henrique Jordão <henriquejordao95@gmail.com>
2022-08-25 22:35:20 +0200

    Add base project

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Henrique Capelatto Jordão on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
